79f7bc2462af64e9d987390b6fe6aebf29a7059f
blackjack605 <jacklwinski@gmail.com>
2016-10-03 17:18:13 -0400

    AddedBackbutton
    Added a back button to the user profile page.

--- src/controller/UserProfileController.java
+++ src/controller/UserProfileController.java
@@ -1,7 +1,25 @@
 package controller;
 
+import fxapp.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
 /**
  * Created by Forrest on 10/3/2016.
  */
 public class UserProfileController {
+
+    @FXML
+    public void backButtonPressed() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("../view/loggedInForm.fxml"));
+        mainApplication.setWindow(loader);
+        LoginController controller = loader.getController();
+        controller.setMainApp(mainApplication);
+    }
+
+    private Main mainApplication;
+    public void setMainApp(Main main) {
+        mainApplication = main;
+    }
 }
