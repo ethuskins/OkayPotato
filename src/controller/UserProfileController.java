00e9c9a21721a0e6953617dbb6f8e07cd6e851e5
Ethaux <alton.nic@gmail.com>
2016-10-03 18:05:10 -0400

    button update
    More buttons work

--- src/controller/UserProfileController.java
+++ src/controller/UserProfileController.java
@@ -1,17 +1,24 @@
 package controller;
 
+import fxapp.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
 /**
  * Controller for the user Profile screen. Allows the user to edit any editable information.
  */
 public class UserProfileController {
-//    @FXML private textfiled something;
-//    @FXML private textfiled something;
-//    @FXML private textfiled something;
-//    @FXML private textfiled something;
-//    @FXML private textfiled something;
-//    @FXML private textfiled something;
-//    @FXML private textfiled something;
-//
-//
+    @FXML
+    public void cancelkButtonPressed() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("../view/loggedInForm.fxml"));
+        mainApplication.setWindow(loader);
+        LoggedinController controller = loader.getController();
+        controller.setMainApp(mainApplication);
+    }
 
+    private Main mainApplication;
+    public void setMainApp(Main main) {
+        mainApplication = main;
+    }
 }
