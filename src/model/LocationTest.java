345cf33bec315d7a254808148a3bdbf5343c86d1
sfbengtson <bengtson94@gmail.com>
2016-11-08 22:10:03 -0500

    JUnit tests-> Location.toString
    Wrote tests for location.toString(). Fixed a bug where a location at the
    equator would result in incorrect formatting

---
+++ src/model/LocationTest.java
@@ -0,0 +1,49 @@
+package model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Forrest on 11/8/2016.
+ */
+public class LocationTest {
+
+    Location location1;
+    Location location2;
+    Location location3;
+    Location location4;
+
+
+    @Before
+    public void setup() throws Exception {
+        location1 = new Location(37.7, 27.5, "test", "test", QuaCondition.SAFE);
+        location2 = new Location(-37.7, 27.5, "test", "test", QuaCondition.SAFE);
+        location3 = new Location(37.7, -27.5, "test", "test", QuaCondition.SAFE);
+        location4 = new Location(0, 0, "test", "test", QuaCondition.SAFE);
+    }
+
+    @org.junit.Test
+    public void testToString() throws Exception {
+        String test = "37.7N, 27.5E";
+        Assert.assertEquals(test, location1.toString());
+    }
+
+    @Test
+    public void testSouth() throws Exception {
+        String test = "37.7S, 27.5E";
+        Assert.assertEquals(test, location2.toString());
+    }
+
+    @Test
+    public void testWest() throws Exception {
+        String test = "37.7N, 27.5W";
+        Assert.assertEquals(test, location3.toString());
+    }
+
+    @Test
+    public void testEquator() throws Exception {
+        String test = "0, 0";
+        Assert.assertEquals(test, location4.toString());
+    }
+}
