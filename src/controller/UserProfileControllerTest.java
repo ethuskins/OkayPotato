76ffb93df129a3798661e34180d3221eb32c6785
ethuskins <ethuskins@gmail.com>
2016-11-10 22:41:23 -0500

    Junit for Populate()
    Created Junit tests for the populate method of UserProfileController

---
+++ src/controller/UserProfileControllerTest.java
@@ -0,0 +1,77 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import junit.framework.Assert;
+import model.AccountType;
+import model.UserProfile;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserProfileControllerTest {
+
+    TextField nameTextField;
+    TextField addressTextField;
+    TextField emailTextField;
+    TextField ageTextField;
+    TextField titleTextField;
+    UserProfile temp = null;
+    @Before
+    public void setUp() throws Exception {
+
+        temp = new UserProfile("Emily", "emily", "whatever", AccountType.USER);
+        temp.setAddress("831 Techwood Drive");
+        temp.setEmailAddress("emily@gatech.edu");
+        temp.setAge(20);
+        temp.setTitle("A title");
+    }
+
+    @Test
+    public void testPopulate() throws Exception {
+        testName();
+        testAddress();
+        testEmail();
+        testAge();
+        testTitle();
+    }
+
+    @Test
+    public void testName() throws Exception {
+        nameTextField.setText(temp.getName());
+        Assert.assertEquals(nameTextField.getText(), temp.getName());
+    }
+
+    @Test
+    public void testAddress() throws Exception {
+        if (temp.getAddress() != null) {
+            addressTextField.setText(temp.getAddress());
+        }
+        Assert.assertEquals(addressTextField.getText(), temp.getAddress());
+    }
+
+    @Test
+    public void testEmail() throws Exception {
+        if (temp.getEmailAddress() != null) {
+            emailTextField.setText(temp.getEmailAddress());
+        }
+        Assert.assertEquals(emailTextField.getText(), temp.getEmailAddress());
+    }
+
+    @Test
+    public void testAge() throws Exception {
+        ageTextField.setText("" + temp.getAge());
+        Assert.assertEquals(ageTextField.getText(), Integer.toString(temp.getAge()));
+    }
+    @Test
+    public void testTitle() throws Exception {
+        if (temp.getTitle() != null) {
+            titleTextField.setText(temp.getTitle());
+        }
+        Assert.assertEquals(titleTextField.getText(), temp.getTitle());
+    }
+
+
+}
